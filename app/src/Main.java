58544ba68c17b161f8fee1fda499c2309efa9fd9
Jakub Trawnicki <kubatrawka@gmail.com>
2022-11-21 13:23:44 +0100

    Dodanie do głównej aplikacji nowej funkcjonalności

--- app/src/Main.java
+++ app/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello World!");
+        while (true) {
+            System.out.println("Podaj liczbę do podania pierwiastka: ");
+            int liczba = scanner.nextInt();
+            System.out.println(Math.sqrt((double) liczba));
+        }
     }
 }
